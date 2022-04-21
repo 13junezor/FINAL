a1400009f5a35858073fa4960b408ce49f126add
Kirill Alekseev <alekseev_kirill_a@mail.ru>
2022-04-21 14:16:31 +0500

    исправляет основную навигацию при уменьшении окна

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -6,25 +6,27 @@ function Head() {
   
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light cap" >
-  <div className="container-fluid cap">
-  <Link className='nav-link but' to="/">Главная</Link>
-    
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-      <li class="nav-item">
-      <Link className='nav-link but' to="signin">Войти</Link>
-      <Link className='nav-link but' to="posts">Посты</Link>
-        <Link className='nav-link but' to="posting">Опубликовать статью</Link>
+  <div className="container">
+      <ul className="navbar-nav d-flex flex-row">
+      <li className="nav-item mx-2 nav-brand">
+      <Link className='nav-link but' to="/">Главная</Link>
         </li>
-        <li className="nav-item">
+        <li className="nav-item mx-2">
+        <Link className='nav-link but' to="signin">Войти</Link>
+        </li >
+    <li className="nav-item mx-2"><Link className='nav-link but' to="posts">Посты</Link>
+    </li>
+      <li className="nav-item mx-2">
+      <Link className='nav-link but' to="posting">Опубликовать статью</Link>
+      </li>
+        <li className="nav-item mx-2">
         <Link className='nav-link but' to="about">О нас</Link>
         </li>
-        <li class="nav-item">
+        <li className="nav-item mx-2">
         <Link className='nav-link but' to="contacts">Контакты</Link>
         </li>
                     </ul>
     </div>
-  </div>
 </nav>
   )
 }
